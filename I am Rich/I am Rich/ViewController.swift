12f95c9eb6ffec7eaed24585bda9e4d2c69a9526
Reethu Thota <reethu.thota@gmail.com>
2023-07-24 16:43:08 +0530

    added I am Rich

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Reethu RG Thota on 24/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
