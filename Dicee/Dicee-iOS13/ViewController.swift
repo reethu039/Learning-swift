d4e8cd7f6779d6dfdb8a9764d18cb276b7792052
Reethu Thota <reethu.thota@gmail.com>
2023-08-05 14:43:00 +0530

    IBOutlets & changing UI elements

--- Dicee/Dicee-iOS13/ViewController.swift
+++ Dicee/Dicee-iOS13/ViewController.swift
@@ -10,12 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //IBOutlet helps in referencing an UI element
     @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        //WHO.WHAT = VALUE
         diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
+        diceImageView1.alpha = 0.5 //alpha is the transparency
+        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
     }
 
 
